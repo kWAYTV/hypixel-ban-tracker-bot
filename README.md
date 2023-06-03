7c6391ce9a766b1aeaff9328faa609465c53d41a
kWAY <55437151+kWAYTV@users.noreply.github.com>
2023-06-03 19:04:41 +0200

    Release

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# hypixel-ban-tracker-bot
- Simple discord bot to track hypixel bans.
+# 🏰 HypixelBanChecker 🛡️
+
+Welcome to the HypixelBanChecker, a discord bot that actively monitors and reports bans on the Hypixel Minecraft server. The bot is intended to assist administrators and players by providing real-time ban data fetched directly from the Hypixel API. It's designed to work seamlessly with your existing discord channels.
+
+## 📖 About
+
+HypixelBanChecker tracks watchdog (automatic) and staff (manual) bans on the Hypixel Minecraft server. The bot is easy to set up and use, and it offers real-time updates on the ban status of players on the Hypixel server.
+
+## 🚀 Features
+
+1. **Real-time Updates** - The bot checks the ban status every 0.1 seconds.
+2. **Easy to Use Commands** - Subscribing or unsubscribing a channel from the ban updates is as simple as running a command.
+3. **Embeds for Better Visualization** - The bot uses Discord Embeds for a better visual presentation of ban data.
+
+## 🛠️ Setup
+
+1. Clone the repository & install the requirements.
+2. Update the `config.json` file with your discord token and the channel IDs where you want the bot to send updates.
+3. Run the bot!
+
+```bash
+python main.py
+```
+
+## 🖐️ Commands
+
+- `subscribe` - Subscribes the channel which the command is ran in to the Ban Tracker.
+- `unsubscribe` - Unsubscribes the channel which the command is ran in to the Ban Tracker.
+
+## ⚠️ Disclaimer
+
+This is a Proof of Concept (PoC) project. It is not officially affiliated with, maintained, sponsored or endorsed by Hypixel or Mojang/Microsoft. Use this tool responsibly and respect the Hypixel server rules and terms.
---
+++ main.py
@@ -0,0 +1,119 @@
+import math
+import json
+import time
+import discord
+import logging
+import requests as reqs
+from itertools import cycle
+from discord.ext import tasks
+
+class JSONConfig:
+    def __init__(self, file_name: str) -> None:
+        self.file_name = file_name
+        with open(file_name) as conf:
+            self.config = json.load(conf)
+
+    def update(self, key: str, value: any):
+        self.config[key] = value
+        with open(self.file_name, "w") as conf:
+            json.dump(self.config, conf, indent=4)
+
+class BanTracker:
+    def __init__(self) -> None:
+        self.owd_bans = None
+        self.ostaff_bans = None
+        self.session = reqs.Session()
+        self.session.headers.update({"User-Agent": "i am a professional boy kisser"}) #The user agent can be anything, plancke only blocks the default requests user agent
+        self.img_logo = "https://i.imgur.com/qXuAK9O.png"
+
+    def check_bans(self):
+        curr_stats = self.session.get(
+            'https://api.plancke.io/hypixel/v1/punishmentStats'
+        ).json().get('record')
+        wd_bans = curr_stats.get("watchdog_total")
+        staff_bans = curr_stats.get("staff_total")
+        embeds = []
+
+        if self.owd_bans != None and self.ostaff_bans != None:
+            wban_dif = wd_bans - self.owd_bans
+            sban_dif = staff_bans - self.ostaff_bans
+
+            if wban_dif > 0:
+                embed = discord.Embed(
+                    color=discord.Color.from_rgb(247, 57, 24),
+                    description=f"<t:{math.floor(time.time())}:R>",
+                ).set_author(name=f"Watchdog banned {wban_dif} player{'s'[:wban_dif^1]}!").set_thumbnail(url=self.img_logo).set_footer(text="Hypixel Ban Tracker | .gg/kws")
+                embeds.append(embed)
+
+            if sban_dif > 0:
+                embed = discord.Embed(
+                    color=discord.Color.from_rgb(247, 229, 24),
+                    description=f"<t:{math.floor(time.time())}:R>",
+                ).set_author(name=f"Staff banned {sban_dif} player{'s'[:sban_dif^1]}!").set_thumbnail(url=self.img_logo).set_footer(text="Hypixel Ban Tracker | .gg/kws")
+                embeds.append(embed)
+
+        self.owd_bans = wd_bans
+        self.ostaff_bans = staff_bans
+        return embeds
+
+class BanTrackerBot(discord.Client):
+    def __init__(self, intents: discord.Intents) -> None:
+        super().__init__(intents=intents)
+        self.tree = discord.app_commands.CommandTree(self)
+        self.jsonconfig = JSONConfig("config.json")
+        self.bantracker = BanTracker()
+        self.logger = logging.getLogger('discord')
+
+        @self.event
+        async def on_ready():
+            self.channel_ids = self.jsonconfig.config.get("channels")
+            for guild in self.guilds:
+                self.tree.copy_global_to(guild=guild)
+                await self.tree.sync(guild=guild)
+            self.logger.info(f"Logged in as {self.user.name}#{self.user.discriminator}.")
+            self.logger.info(f"Synced commands with {len(self.guilds)} guild{'s'[:len(self.guilds)^1]}.")
+            changeStatus.start()
+            check_loop.start()
+
+        @self.event
+        async def on_guild_join(guild):
+            self.tree.copy_global_to(guild=guild)
+            await self.tree.sync(guild=guild)
+            self.logger.info(f"Synced commands with {guild.name}.")
+
+        @self.tree.command()
+        @discord.app_commands.checks.has_permissions(administrator=True)
+        async def subscribe(interaction: discord.Interaction):
+            """Subscribes the channel which the command is ran in to the Ban Tracker"""
+            if interaction.channel_id not in self.channel_ids:
+                self.channel_ids.append(interaction.channel_id)
+                self.jsonconfig.update("channels", self.channel_ids)
+                self.logger.info(f"{interaction.channel.name} in {interaction.guild.name} was subscribed.")
+                await interaction.response.send_message("> This channel is now subscribed.")
+            else:
+                await interaction.response.send_message("> This channel is already subscribed.")
+
+        @self.tree.command()
+        @discord.app_commands.checks.has_permissions(administrator=True)
+        async def unsubscribe(interaction: discord.Interaction):
+            """Unsubscribes the channel which the command is ran in to the Ban Tracker"""
+            self.channel_ids.remove(interaction.channel_id)
+            self.jsonconfig.update("channels", self.channel_ids)
+            self.logger.info(f"{interaction.channel.name} in {interaction.guild.name} was unsubscribed.")
+            await interaction.response.send_message("> This channel is no longer subscribed.")
+
+        # Dynamic activity
+        status = cycle(["Hypixel Bans", "Cheaters", "Hackers", "Rulebreakers", "Banned Players"])
+        @tasks.loop(seconds=30)
+        async def changeStatus():
+            await self.change_presence(status=discord.Status.do_not_disturb, activity=discord.Activity(type=discord.ActivityType.watching, name=next(status)))
+
+        @tasks.loop(seconds=0.1)
+        async def check_loop():
+            bans = self.bantracker.check_bans()
+            if bans:
+                [await self.get_channel(channel_id).send(embed=embed) for embed in bans for channel_id in self.channel_ids]
+
+if __name__ == "__main__":
+    discordbot = BanTrackerBot(discord.Intents.default())
+    discordbot.run(discordbot.jsonconfig.config.get("token"))
